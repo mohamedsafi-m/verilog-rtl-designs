28246d0cc8c08f6302ae3242651a1f6c5153acec
mohamedsafi-m <safimohammed695@gmail.com>
2026-09-15 14:31:16 +0530

    Add tb.v

---
+++ combinational/logic_gates/Buffer/tb.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+module tb;
+reg A;
+wire Y;
+buffer uut(
+.Y(Y),
+.A(A));
+initial
+begin
+$monitor("Time = %0t | A = %d | Y = %b",
+$time, A,Y);
+end
+initial
+begin
+A = 1'b0 ;
+#10;
+A = 1'b1 ;
+#10;
+$finish;
+end
+endmodule
