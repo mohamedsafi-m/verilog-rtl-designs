3a34b245b218997f2ea036c3aa3b45132bec1f11
mohamedsafi-m <safimohammed695@gmail.com>
2026-09-15 14:35:17 +0530

    Add tb.v

---
+++ combinational/logic_gates/Xnor/tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+module tb;
+reg A,B;
+wire Y;
+xnor uut(
+.Y(Y),
+.A(A),
+.B(B));
+initial
+begin
+$monitor("Time = %0t | A = %d B=%d | Y = %b",
+$time, A,B,Y);
+end
+initial
+begin
+A = 1'b0 ; B = 1'b0;
+#10;
+A = 1'b0 ; B = 1'b1;
+#10;
+A = 1'b1 ; B = 1'b0;
+#10;
+A = 1'b1 ; B = 1'b1;
+#10;
+$finish;
+end
+endmodule
