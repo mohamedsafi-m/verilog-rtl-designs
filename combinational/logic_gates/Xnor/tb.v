d8b272ab1b8131a83666d3875cfc9d93b00a6e12
mohamedsafi-m <safimohammed695@gmail.com>
2026-09-18 08:46:08 +0530

    Add Xnor.v and tb.v

--- combinational/logic_gates/Xnor/tb.v
+++ combinational/logic_gates/Xnor/tb.v
@@ -2,7 +2,7 @@
 module tb;
 reg A,B;
 wire Y;
-xnor uut(
+Xnor uut(
 .Y(Y),
 .A(A),
 .B(B));
--- combinational/logic_gates/Xnor/xnor.v
+++ combinational/logic_gates/Xnor/xnor.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module xnor(Y,A,B);
+module Xnor(Y,A,B);
 output Y;
 input A,B;
 assign Y = ~(A ^ B);
