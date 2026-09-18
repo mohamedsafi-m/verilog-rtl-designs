01f19ca1fc4b6828b25f49c44a2133dea2bcb03a
mohamedsafi-m <safimohammed695@gmail.com>
2026-09-18 08:45:59 +0530

    Add Xor.v and tb.v

--- combinational/logic_gates/Xor/tb.v
+++ combinational/logic_gates/Xor/tb.v
@@ -2,7 +2,7 @@
 module tb;
 reg A,B;
 wire Y;
-xor uut(
+Xor uut(
 .Y(Y),
 .A(A),
 .B(B));
--- combinational/logic_gates/Xor/xor.v
+++ combinational/logic_gates/Xor/xor.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module xor(Y,A,B);
+module Xor(Y,A,B);
 output Y;
 input A,B;
 assign Y = A ^ B;
