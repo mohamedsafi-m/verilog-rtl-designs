7b67367659218acc006df7036cfcd6f8372bee43
mohamedsafi-m <safimohammed695@gmail.com>
2026-09-18 08:45:32 +0530

    Add Nor.v and tb.v

--- combinational/logic_gates/Nor/nor.v
+++ combinational/logic_gates/Nor/nor.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module nor(Y,A,B);
+module Nor(Y,A,B);
 output Y;
 input A,B;
 assign Y =~(A | B);
--- combinational/logic_gates/Nor/tb.v
+++ combinational/logic_gates/Nor/tb.v
@@ -2,7 +2,7 @@
 module tb;
 reg A,B;
 wire Y;
-nor uut(
+Nor uut(
 .Y(Y),
 .A(A),
 .B(B));
